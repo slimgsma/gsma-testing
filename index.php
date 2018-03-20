fabed44361ff67487f3a7a6f21377e535b12c3da
slimgsma <slim@gsma.com>
2018-03-20 10:17:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<?php
-    echo "for nothing is means nothing";
-?>  
+<script type="text/javascript">
+    		jQuery(document).ready(function($){ 
+				//GSMA_phase2 - more information zone - Top & Bottom
+                $(".sub-menu .menu-item a").attr('onclick', 'lbReload(\'CN_GSMA_2018phase2_more_information_zone\',\'\',\'\',\'\');');
+       
+			});
+		</script>
+
+<div class="menu-top">
+                <div class="menu">
+                <ul><li id="menu-item-2078" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-2078"><a href="http://y18.mwcshanghai.cn/attend/">参会</a>
+<ul class="sub-menu">
+	<li id="menu-item-2108" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2108"><a href="http://y18.mwcshanghai.cn/attend/register/">登记</a></li>
+	<li id="menu-item-2101" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2101"><a href="http://y18.mwcshanghai.cn/attend/location-venues/">地点及会场信息</a></li>
+	<li id="menu-item-2114" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2114"><a href="http://y18.mwcshanghai.cn/attend/hotels-travel/">酒店及抵达信息</a></li>
+	<li id="menu-item-2128" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2128"><a href="http://y18.mwcshanghai.cn/attend/press/">媒体/分析师专区</a></li>
+	<li id="menu-item-2809" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2809"><a href="http://y18.mwcshanghai.cn/about-the-gsma/mobile-world-capital-barcelona/">世界移动之都巴塞罗那</a></li>
+	<li id="menu-item-2810" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2810"><a href="/attend/hotels-travel/">Hotels and Travel Short</a></li>
+</ul>
+</li>
+                    </ul>
+    </div>
